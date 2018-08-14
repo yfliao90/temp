bc0de7ed6db900cb2e560812474902f61cab634b
unknown <amy.liao@dfrobot.com>
2018-08-14 13:59:26 +0800

    index.js 有问题

--- controllers/index.js
+++ controllers/index.js
@@ -3,15 +3,20 @@ var fs = require('fs');
 var fn_index = async (ctx, next) => {
     fs.readFile('index.html', 'utf-8', (err, data) => {
     if (err) throw err;
-    console.log(data);
-    ctx.response.body = data;
-    // ctx.response.body = `<h1>Index</h1>
-    //     <form action="/signin" method="post">
-    //         <p>Name: <input name="name" value="koa"></p>
-    //         <p>Password: <input name="password" type="password"></p>
-    //         <p><input type="submit" value="Submit"></p>
-    //     </form>`;
+    ctx.response.body = `<h1>Index</h1>
+    <form action="/signin" method="post">
+        <p>Name: <input name="name" value="koa"></p>
+        <p>Password: <input name="password" type="password"></p>
+        <p><input type="submit" value="Submit"></p>
+    </form>`
+    next();
     });
+    // ctx.response.body = `<h1>Index</h1>
+    // <form action="/signin" method="post">
+    //     <p>Name: <input name="name" value="koa"></p>
+    //     <p>Password: <input name="password" type="password"></p>
+    //     <p><input type="submit" value="Submit"></p>
+    // </form>`;
 };
 
 module.exports = {
